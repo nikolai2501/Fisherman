79ca76681f7b402bc2415eadbbacc3032fbc4c43
Uzunow <125346134+Uzunow@users.noreply.github.com>
2025-04-30 11:26:41 +0300

    Delete Captain.cs

--- Captain.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FishingPlase.Models
-{
-    public class Captain
-    {
-        public object Name { get; internal set; }
-        public object Qualifications { get; internal set; }
-    }
-}
